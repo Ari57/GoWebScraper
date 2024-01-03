ef3208df202417f3411563ebe68f681b4914c8a2
Nathan Devey-Skeete <njds1676@gmail.com>
2024-01-03 10:30:26 +0000

    Updated code to get the title and price for computers

--- src/main.go
+++ src/main.go
@@ -8,14 +8,34 @@ import (
 )
 
 func main() {
+	title := ""
+	price := ""
+
 	c := colly.NewCollector()
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
-		if strings.Contains(link, "computer") || strings.Contains(link, "phone") {
-			fmt.Println(e.Request.AbsoluteURL(link))
+		if strings.Contains(link, "computer") {
+			c.Visit(e.Request.AbsoluteURL(link))
+
+			c.OnHTML(".wrapper", func(e *colly.HTMLElement) {
+				e.ForEach("h4", func(_ int, p *colly.HTMLElement) {
+					p.Text = strings.Replace(p.Text, "\t", "", -1)
+					p.Text = strings.Replace(p.Text, "\n", "", -1)
+
+					if strings.Contains(p.Text, "$") {
+						price = p.Text
+					} else {
+						p.Text = strings.Replace(p.Text, "...", "", -1)
+						title = p.Text
+					}
+
+					product := title + " - " + price
+					fmt.Println(product)
+				})
+
+			})
 		}
-		// c.Visit(e.Request.AbsoluteURL(link))
 	})
 
 	// c.OnRequest(func(r *colly.Request) {
